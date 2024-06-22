eb5966f8469de0eef7219801a7db1ab84ab63e0b
Juliano <JulianoMarthins@Yahoo.com.br>
2024-06-21 22:17:32 -0300

    Adicionar arquivos de projeto.

---
+++ ContaBancária/Cliente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancária {
+    internal class Cliente {
+
+        public string nome{ get; set; }
+        private double saldo;
+        private int numConta = 1001;
+
+
+        public Cliente(string nome, double deposito) {
+            this.nome = nome;
+            this.saldo = deposito;
+            this.numConta = this.numConta += 10;
+        }
+
+        public double Deposito(double valor) {
+            this.saldo += valor;
+            return this.saldo;
+
+        }
+
+        public double Saque(double valor) {
+
+            if(this.saldo >= valor + 5.0){
+                this.saldo -= (valor + 5.0);
+            }
+            else {
+                this.saldo -= (valor + 5.0) ;
+            }
+
+            return this.saldo;
+        }
+
+        public string ToString() {
+            string saldoFormatado = string.Format(CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", this.saldo);
+            return $"\n\n<<<<<Extrato>>>>>\nNúmero da conta: {numConta}\nNome: {this.nome}\nSaldo: {saldoFormatado}\n";
+        }
+
+    }
+}
---
+++ ContaBancária/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancária {
+    internal class Program {
+        static void Main(string[] args) {
+
+       
+
+            Console.WriteLine("   Programado por JulianoMarthins\n\n");
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+
+            Console.WriteLine("Digite os dados do cliente: ");
+            
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Deseja realizar deposito inicial: [S/N] ");
+            string opcao = Console.ReadLine().ToLower();
+
+
+            double valor = 0.0;
+            if(opcao == "s") {
+                Console.Write("\nDigite o valor de deposito: ");
+                valor = double.Parse(Console.ReadLine());
+
+            }
+         
+
+            Cliente cliente = new Cliente(nome, valor);
+
+            Console.WriteLine("\nCliente cadastrado com sucesso");
+            Console.WriteLine(cliente.ToString());
+
+            Console.Write("\nDigite o valor para deposito: ");
+            valor = double.Parse(Console.ReadLine());
+
+            cliente.Deposito(valor);
+            Console.WriteLine("\n" + cliente.ToString());
+
+            Console.WriteLine("\nDigite o valor de saque: ");
+            valor = double.Parse(Console.ReadLine());
+
+            cliente.Saque(valor);
+
+            Console.WriteLine("\n" + cliente.ToString());
+
+
+
+        }
+    }
+}
